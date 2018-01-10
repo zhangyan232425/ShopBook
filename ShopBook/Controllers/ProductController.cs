a8b4e86705a8329af827aa1fd178a4e320d86bd6
yan <zhangyan232425@hotmail.com>
2018-01-09 20:14:55 -0500

    trash

--- ShopBook/Controllers/ProductController.cs
+++ ShopBook/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShopBook.Context;
+using ShopBook.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,7 +21,8 @@ namespace ShopBook.Controllers
         
         public async Task<IActionResult> Index(string sort,string search)
         {
-            ViewData["NameSortParm"] = sort=="name_asc"?"name_desc":"name_asc";
+            
+            /* ViewData["NameSortParm"] = sort=="name_asc"?"name_desc":"name_asc";
             ViewData["PriceSortParm" ]= sort== "price_asc" ? "price_desc":"price_asc";
             ViewData["DateSortParm"] = sort=="date_asc" ? "date_desc":"date_asc";
             
@@ -29,8 +31,31 @@ namespace ShopBook.Controllers
             if (!String.IsNullOrEmpty(search))
             {
                 product = product.Where(p=>p.Name.Contains(search));
+            } */
+            public PaginatedList<Product> Product {get;set;}
+            public async Task OnGetAsync(string sort,string filter,string search,int? pageIndex)
+            {
+                CurrentSort = sort;
+                NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                DateSort = sortOrder =="Date" ? "date_desc":"Date";
+                
+                if (search!=null)
+                {
+                    pageIndex=1;
+                }
+                else
+                {
+                    search = filter;
+                }
+
+                filter = search;
+                IQueryable<Product> product = from item in _context.Products
+                select item;
+                if(!String.IsNullOrEmpty(search))
+                {
+                    product = product.Where(p=>p.Name.Contains(search));
+                }
             }
-            
             switch(sort)
             {
                 case "name_desc":
@@ -52,9 +77,9 @@ namespace ShopBook.Controllers
                 case "date_asc":
                     product = product.OrderBy(p => p.ProductDate);
                     break;
-                
-
             }
+            int pageSize =3;
+            Product = await PaginatedList<Product>.CreateAsync(product.AsNoTracking(),pageIndex??1,pageSize)
             return View(await product.AsNoTracking().ToListAsync());
         }
     }
---
+++ ShopBook/PaginatedList.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace ShopBook
+{
+    public class PaginatedList<T>:List<T>
+    {
+        public int PageIndex { get; private set;}
+        public int TotalPages { get; private set;}
+        
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count/(double)pageSize);
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex>1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex<TotalPages);
+            }
+        }
+
+        public static async Task<PaginatedList<T>> CreatAsync(IQueryable<T> source, int pageIndex. int pageSize)
+        {
+             var count = await source.CoountAsync();
+             var items = await source.Skip((pageIndex -1)*pageSize).Take(pageSize).ToListAsync();
+             return new PaginatedList<T>(items,count,pageIndex,pageSize);
+        }
+
+    }
+ 
+
+
+
+}
